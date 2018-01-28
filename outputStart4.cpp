4523ec7ce941bfa9fd018b4a273266d814316972
parkjaeseong <tmakdlfwotjd@naver.com>
2018-01-28 15:46:56 -0800

    outputStart1to7

---
+++ outputStart4.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+
+	int n;
+	scanf("%d",&n);
+
+	for(int i = 0; i <= n-1; i++)
+	{
+
+		for(int j = 0; j<i; j++)
+		{
+			printf(" ");
+		}
+
+		for(int j = n; j > i; j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
---
+++ outputStart7.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(void)
+{
+
+	int n;
+	scanf("%d",&n);
+
+	for(int i = 0; i <= n-1; i++)
+	{
+		for(int j = 0; j < n - i - 1; j++)
+		{
+			printf(" ");
+		}
+
+		for(int j = 0; j < i * 2 + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	for(int i = 1; i <= n-1; i++)
+	{
+		for(int j = 0; j < i ; j++)
+		{
+			printf(" ");
+		}
+
+		for(int j = (n-i) * 2 + 1; j > 2; j--)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
