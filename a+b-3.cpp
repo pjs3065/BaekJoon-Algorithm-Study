a26a26fa3207eddf48bbbc5ca34bcb2eee68daca
Jaeseong Park(JSP) <tmakdlfwotjd@naver.com>
2018-04-06 03:01:38 +0900

    modified a+b-3.cpp

--- a+b-3.cpp
+++ a+b-3.cpp
@@ -2,18 +2,15 @@
 int main(void)
 {
 	int n,a,b;
-	int sum[100] = {0,};
+	int sum;
+
 	scanf("%d",&n);
-
+	
 	for(int i = 0; i < n; i++)
 	{
-		scanf("%d %d",&a, &b);
-		sum[i] = a + b;
-	}
-
-	for(int i = 0; i < n; i++)
-	{
-		printf("%d\n", sum[i]);
+		scanf("%d %d",&a,&b);
+		sum = a + b;
+		printf("%d\n", sum);
 	}
 	return 0;
 }
