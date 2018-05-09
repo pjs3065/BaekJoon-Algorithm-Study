a2d072c7e224a6821ac5032caa3ff5a14cf27217
Jaeseong Park(JSP) <tmakdlfwotjd@naver.com>
2018-05-09 22:39:30 +0900

    subset_shortlength.cpp

--- subset_shortlength.cpp
+++ subset_shortlength.cpp
@@ -8,12 +8,12 @@ int main()
 	int n, s;
 	int sum = 0;
 	int left = 0, right = 0;
-	int min_length = 1000001;
+	int min_length = 1000002;
 
 	//입력
 	scanf("%d %d",&n, &s);
 
-	vector<int> subset(n);
+	vector<int> subset(n + 1);
 
 	//부분합 입력
 	for(int i = 0; i < n; i++)
@@ -21,34 +21,22 @@ int main()
 		scanf("%d",&subset[i]);
 	}
 
-	sum = subset[left] + subset[right];
+	sum = subset[0];
 
-	while(1)
+	while(right < n)
 	{
 		//조건과 합이 같을 경우
 		if(s == sum)
 		{
-			cout << "dd" << right << ' ' << left << '\n';
 			//최소값 길이에 넣어준다
 			if(min_length > right - left)
 			{
-				min_length = right - left;
+				min_length = right - left + 1;
 			}
 
 			//오른쪽으로 이동하게 하고
 			right ++;
-
-			//오른쪽이 n보다 크거나 같으면 끝
-			if(right >= n)
-			{
-				break;
-			}
-
-			//아니면 이동한 오른쪽 subset과 더한다.
-			else
-			{
-				sum += subset[right];
-			}
+			sum += subset[right];
 		}
 
 		//조건보다 합이 작을 경우
@@ -56,37 +44,24 @@ int main()
 		{
 			//오른쪽으로 이동시키고
 			right++;
-
-			//위 조건과 동일하다
-			if(right >= n)
-			{
-				break;
-			}
-
-			else
-			{
-				sum += subset[right];
-			}
+			sum += subset[right];
 		}
 
 		//조건보다 합이 클 경우
 		else if(s < sum)
 		{
+			//최소값 길이에 넣어준다
+			if(min_length > right - left)
+			{
+				min_length = right - left + 1;
+			}
+
 			//먼저 left와 right가 같은지 확인한후 같으면
 			if(left == right)
 			{
 				//위의 조건과 동일하게 움직이며
 				right ++;
-
-				if(right >= n)
-				{
-					break;
-				}
-
-				else
-				{
-					sum += subset[right];
-				}
+				sum += subset[right];
 			}
 
 			//아닐 경우에는
@@ -100,7 +75,7 @@ int main()
 	}
 
 	//min_length가 그대로일 경우 0으로 변환
-	if(min_length ==  1000001)
+	if(min_length ==  1000002)
 	{
 		min_length = 0;
 	}
