d21393f17ebfdca5f56f1d4aad6ccb8fa6b67c54
ParkJaeseong <tmakdlfwotjd@naver.com>
2018-05-22 02:34:52 +0900

    (imcomplete) han_number.cpp

---
+++ han_number.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+	int han[1001] = {0,};
+	int n;
+	scanf("%d", &n);
+
+	if(n < 100)
+	{
+		printf("%d\n", n);
+		return 0;
+	}
+
+	int m = 100;
+
+	for(int i = 100; i <= n; i++)
+	{
+		for(int i = 
+
+
+
+
+	}
+
+}
