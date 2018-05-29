a279f746b704c9ec9f3522a103759f474be879a3
ParkJaeseong <tmakdlfwotjd@naver.com>
2018-05-29 23:59:11 +0900

    dial.cpp 1. short coding

--- dial.cpp
+++ dial.cpp
@@ -1,48 +1,17 @@
 #include<stdio.h>
-#include<iostream>
-#include<string>
-
-using namespace std;
 
 int main()
 {
-	string dial;
-	cin >> dial;
-
-	int length = dial.length();
+	char c;
 	int timer = 0;
 
-	for(int i = 0; i < length; i++)
+	while(scanf("%c",&c) == 1 && c != '\n')
 	{
-		switch (dial[i])
-		{
-		case 'A':
-		case 'B':
-		case 'C': timer+= 3; break;
-		case 'D':
-		case 'E':
-		case 'F': timer+= 4; break;
-		case 'G':
-		case 'H':
-		case 'I': timer+= 5; break;
-		case 'J':
-		case 'K':
-		case 'L': timer+= 6; break;
-		case 'M':
-		case 'N':
-		case 'O': timer+= 7; break;
-		case 'P':
-		case 'Q':
-		case 'R':
-		case 'S': timer+= 8; break;
-		case 'T':
-		case 'U':
-		case 'V': timer+= 9; break;
-		case 'W':
-		case 'X':
-		case 'Y':
-		case 'Z': timer+= 10; break;
-		}
+		if(c == 'Z') c--;
+		if(c >= 'S') c--;
+
+		int num = (c-'A')/3 + 3;
+		timer += num;
 	}
 
 	printf("%d\n", timer);
