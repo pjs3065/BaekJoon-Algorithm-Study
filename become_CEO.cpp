307f56d1a5ec38ae68e0d39fa9d6637eba7dfc1a
ParkJaeseong <tmakdlfwotjd@naver.com>
2018-07-09 17:22:20 +0900

    become_CEO.cpp

--- become_CEO.cpp
+++ become_CEO.cpp
@@ -1,47 +1,19 @@
-#include<stdio.h>
-#include<vector>
-
-using namespace std;
-
+#include<cstdio>
+int t,k,n,room[15];
 int main()
 {
-	int t, count = 1;
 	scanf("%d", &t);
-
-
-	while(count <= t)
+	while(t--)
 	{
-		int k, n;
 		scanf("%d %d", &k,&n);
-
-		vector< vector<int> > building;
-
-		for(int i = 0; i <= k; i++)
-		{
-			vector<int> room(n + 1);
-			building.push_back(room);
-		}
-
-		for(int i = 0; i <= k; i++)
-		{
-			building[k-i][1] = 1; 
-		}
-
-		for(int j = 1; j <= n; j++)
-		{
-			building[k][j] = j;
-		}
-
-		for(int i = 1; i <= k; i++)
+		for(int i = 1; i <= n; i++) room[i] = i;
+		for(int i = 0; i < k; i++)
 		{
 			for(int j = 2; j <= n; j++)
 			{
-				building[k-i][j] = building[k-i][j-1] + building[k-i+1][j];
+				room[j] += room[j-1];  
 			}
 		}
-
-		printf("%d\n",building[0][n]);
-		count++;
+		printf("%d\n",room[n]);
 	}
-	return 0;
 }
