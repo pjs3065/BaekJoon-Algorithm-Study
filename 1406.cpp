153862387e73288ed5dd2e9085665899717600c9
Jaeseong Park(JSP) <tmakdlfwotjd@naver.com>
2018-08-07 20:24:27 +0900

    (complete) 1406.cpp 에디터

--- 1406.cpp
+++ 1406.cpp
@@ -1,43 +1,86 @@
-#include<iostream>
-#include<string>
+#include<cstdio>
+#include<cstring>
 #include<stack>
 
 using namespace std;
 
 int main()
 {
-	stack<char>editor;
+	//문자열 입력
+	char str[600000];
+	scanf("%s",str);
 
-	string str;
-	cin >> str;
+	//스택에 입력
+	stack<char> left;
+	stack<char> right;
+
+	int len = strlen(str);
+
+	for(int i = 0; i < len; i++)
+	{
+		left.push(str[i]);
+	}
 
 	int n;
-	cin >> n;
+	scanf("%d",&n);
 
-	for(int i = 0; i < n; i++)
+	//operation
+	while(n--)
 	{
 		char f;
-		scanf("%c", &f);
+		scanf(" %c", &f);
 
-		if(f == 'P')
+		if(f == 'L')
 		{
-			char s;
-			scanf("%c", &s);
-
+			if(!left.empty())
+			{
+				right.push(left.top());
+				left.pop();
+			}
 		}
-		else if (f == 'L')
-		{
-		
-		}
-		else if (f == 'D')
-		{
 
-		}
-		else if (f == 'B')
+		else if(f == 'D')
 		{
+			if(!right.empty())
+			{
+				left.push(right.top());
+				right.pop();
+			}
+		}
 
+		else if(f == 'B')
+		{
+			if(!left.empty())
+			{
+				left.pop();
+			}
+		}
+
+		else if(f == 'P')
+		{
+			char word;
+			scanf(" %c", &word);
+
+			left.push(word);
 		}
 	}
+
+	//출력
+
+	while(!left.empty())
+	{
+		right.push(left.top());
+		left.pop();
+	}
+
+	while(!right.empty())
+	{
+
+		printf("%c",right.top());
+		right.pop();
+	}
+
+	printf("\n");
 
 	return 0;
 }
