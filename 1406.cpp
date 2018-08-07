be7c10a06d1919751597ee7bed5da8b8689374f1
ParkJaeseong <tmakdlfwotjd@naver.com>
2018-08-07 17:10:37 +0900

    (imcomplete) 1406.cpp 에디터

---
+++ 1406.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+	stack<char>editor;
+
+	string str;
+	cin >> str;
+
+	int n;
+	cin >> n;
+
+	for(int i = 0; i < n; i++)
+	{
+		char f;
+		scanf("%c", &f);
+
+		if(f == 'P')
+		{
+			char s;
+			scanf("%c", &s);
+
+		}
+		else if (f == 'L')
+		{
+		
+		}
+		else if (f == 'D')
+		{
+
+		}
+		else if (f == 'B')
+		{
+
+		}
+	}
+
+	return 0;
+}
